87c0500294cf3677e184855efd73adbd77d167b6
85589661@qq.com <dc123456>
2020-08-29 15:33:07 +0800

    godeps to gomod

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/codegangsta/cli"
 
-	"github.com/yudai/gotty/backend/localcommand"
-	"github.com/yudai/gotty/pkg/homedir"
-	"github.com/yudai/gotty/server"
-	"github.com/yudai/gotty/utils"
+	"github.com/wenxinnnnn/gotty/backend/localcommand"
+	"github.com/wenxinnnnn/gotty/pkg/homedir"
+	"github.com/wenxinnnnn/gotty/server"
+	"github.com/wenxinnnnn/gotty/utils"
 )
 
 func main() {
--- server/server.go
+++ server/server.go
@@ -18,9 +18,9 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
 
-	"github.com/yudai/gotty/pkg/homedir"
-	"github.com/yudai/gotty/pkg/randomstring"
-	"github.com/yudai/gotty/webtty"
+	"github.com/wenxinnnnn/gotty/pkg/homedir"
+	"github.com/wenxinnnnn/gotty/pkg/randomstring"
+	"github.com/wenxinnnnn/gotty/webtty"
 )
 
 // Server provides a webtty HTTP endpoint.
